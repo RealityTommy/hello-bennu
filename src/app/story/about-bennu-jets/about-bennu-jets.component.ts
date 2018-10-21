70fb0eafabc727f8dbbb97d9cedd1ba2e6f02b4a
Tommy Truong <ttruong246@gmail.com>
2018-10-21 07:58:55 -0400

    Added portfolio

---
+++ src/app/story/about-bennu-jets/about-bennu-jets.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'story-about-bennu-jets',
+  templateUrl: './about-bennu-jets.component.html'
+})
+export class AboutBennuJetsComponent { }
--- src/app/story/story.module.ts
+++ src/app/story/story.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 
 // Import story page components here
 import { AboutBennuComponent } from './about-bennu/about-bennu.component';
+import { AboutBennuJetsComponent } from './about-bennu-jets/about-bennu-jets.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { KnowledgeJourneyComponent } from './knowledge-journey/knowledge-journey.component';
 import { ReferencesComponent } from './referenes/references.component';
@@ -15,6 +16,7 @@ import { SurvivalComponent } from './survival/suvival.component';
 
 const declarations = [
     AboutBennuComponent,
+    AboutBennuJetsComponent,
     IntroductionComponent,
     KnowledgeJourneyComponent,
     ReferencesComponent,
